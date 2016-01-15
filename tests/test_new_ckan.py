f455e2471ab792755e1a9b85509eff7c7ff87ae4
Ian Ward <ian@excess.org>
2016-01-15 18:42:06 -0500

    tests: fix clean-up

--- tests/test_new_ckan.py
+++ tests/test_new_ckan.py
@@ -10,8 +10,8 @@ class TestNewCKAN(object):
 
     def tearDown(self):
         sys.path.remove(HERE + '/new_ckan')
-        for m in sys.modules:
-            if m.startswith('ckan.'):
+        for m in list(sys.modules):
+            if m.startswith('ckan.', 'ckantoolkit'):
                 del sys.modules[m]
 
     def test_import_ckan(self):
